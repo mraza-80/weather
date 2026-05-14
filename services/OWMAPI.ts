faf9b704fd9ed4e45839a5ddf7b907fc9911a8a2
Mohsin Raza <mohsin.raza@fagerstrombolagen.se>
2026-05-14 15:41:57 +0200

    first commit

--- services/OWMAPI.ts
+++ services/OWMAPI.ts
@@ -1,52 +1,38 @@
-/***
- * 
- * Open weather Map API
- * 
- * <https :// openweathermap.org/current>
- * http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
- * https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
- * 
- */
+import axios from "axios";
+import type { OWM_CurrentWeather } from "./OWMAPI.types";
 
-import  axios from "axios"
-import {  type OWM_CurrentWeather } from "./OWMAPI.types";
-const API_KEY=import.meta.env.VITE_OWM_APIKEY;
+const API_KEY = import.meta.env.VITE_OWM_APIKEY;
 
-const BASE_URL="https://api.openweathermap.org/data/2.5"
-const FAKE_SLOW_API =true ;
-const FAKE_SLOW_API_DELAY=2000;
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-/***
- * @param city city name
- * 
- * @return Current weather condition
- * 
- * 
- */
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 2000;
 
-export const getCurrentWeather=async( city:string)=>
-//Query API for current weather conditions in 'city'
-  {
-const res=await axios.get<OWM_CurrentWeather>(BASE_URL+ "/weather",
-{
-    
-    params:{
-            q:city,
-            units:"metric",
-             appid:API_KEY
-     },     
-    });
 
-//Fake slow API
-  
 
-        if(FAKE_SLOW_API)
-        {
-            await new Promise(r=>setTimeout(r,FAKE_SLOW_API_DELAY));
-        }
+export const getCurrentWeather = async (city: string) => {
+  // Query API for current weather conditions in city
 
-        // return current weather conditions 
+  const res = await axios.get<OWM_CurrentWeather>(
+    BASE_URL + "/weather",
+    {
+      params: {
+        q: city,
+        units: "metric",
+        appid: API_KEY,
+      },
+    }
+  );
 
-        return res.data;
+  // Fake slow API
 
-}
+  if (FAKE_SLOW_API) {
+    await new Promise((r) =>
+      setTimeout(r, FAKE_SLOW_API_DELAY)
+    );
+  }
+
+  // return current weather conditions
+
+  return res.data;
+};
